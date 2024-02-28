f06c54ca4cb3f3698a55516454950ec4ab086ea6
brokoli <bocalist@gmail.com>
2024-02-27 22:25:17 -0700

    Completed the plus function

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -4,20 +4,31 @@
     public partial class MainPage : ContentPage
     {
         private string _mainDisplayText;
-        private int _firstNumber;
-        private int _secondNumber;
         private int _totalNumber;
         private string _calculatingMethod;
+        private bool _isFirstNumber;
 
         // This is the constructor for the MainPage class.
         public MainPage()
         {
             InitializeComponent();
             Shell.SetNavBarIsVisible(this, false);
+
+            Reset();
+        }
+
+        // This method is used to reset the calculator app.
+        private void Reset()
+        {
+            _mainDisplayText = "";
+            _totalNumber = 0;
+            _calculatingMethod = "";
+            _isFirstNumber = true;
+            UpdateDisplay();
         }
 
         // This method is used to update the display with the current mainDisplayText value.
-        public void updateDisplay()
+        private void UpdateDisplay()
         {
             mainDisplay.Text = _mainDisplayText;
         }
@@ -25,116 +36,141 @@
         // This method is used to handle the event when the number buttons are clicked.
         private void NumberButton1_Clicked(object sender, EventArgs e)
         {
-            _mainDisplayText += "1";
-            updateDisplay();
+            _mainDisplayText = _isFirstNumber ? "1": _mainDisplayText + "1";
+            _isFirstNumber = !_isFirstNumber && _isFirstNumber;
+            UpdateDisplay();
 
         }
 
         private void NumberButton2_Clicked(object sender, EventArgs e)
         {
-            _mainDisplayText += "2";
-            updateDisplay();
+            _mainDisplayText = _isFirstNumber ? "2": _mainDisplayText + "2";
+            _isFirstNumber = !_isFirstNumber && _isFirstNumber;
+            UpdateDisplay();
 
         }
 
         private void NumberButton3_Clicked(object sender, EventArgs e)
         {
-            _mainDisplayText += "3";
-            updateDisplay();
+            _mainDisplayText = _isFirstNumber ? "3": _mainDisplayText + "3";
+            _isFirstNumber = !_isFirstNumber && _isFirstNumber;
+            UpdateDisplay();
 
         }
 
         private void NumberButton4_Clicked(object sender, EventArgs e)
         {
-            _mainDisplayText += "4";
-            updateDisplay();
+            _mainDisplayText = _isFirstNumber ? "4": _mainDisplayText + "4";
+            _isFirstNumber = !_isFirstNumber && _isFirstNumber;
+            UpdateDisplay();
 
         }
 
         private void NumberButton5_Clicked(object sender, EventArgs e)
         {
-            _mainDisplayText += "5";
-            updateDisplay();
+            _mainDisplayText = _isFirstNumber ? "5": _mainDisplayText + "5";
+            _isFirstNumber = !_isFirstNumber && _isFirstNumber;
+            UpdateDisplay();
 
         }
 
         private void NumberButton6_Clicked(object sender, EventArgs e)
         {
-            _mainDisplayText += "6";
-            updateDisplay();
+            _mainDisplayText = _isFirstNumber ? "6": _mainDisplayText + "6";
+            _isFirstNumber = !_isFirstNumber && _isFirstNumber;
+            UpdateDisplay();
 
         }
 
         private void NumberButton7_Clicked(object sender, EventArgs e)
         {
-            _mainDisplayText += "7";
-            updateDisplay();
+            _mainDisplayText = _isFirstNumber ? "7": _mainDisplayText + "7";
+            _isFirstNumber = !_isFirstNumber && _isFirstNumber;
+            UpdateDisplay();
 
         }
 
         private void NumberButton8_Clicked(object sender, EventArgs e)
         {
-            _mainDisplayText += "8";
-            updateDisplay();
+            _mainDisplayText = _isFirstNumber ? "8": _mainDisplayText + "8";
+            _isFirstNumber = !_isFirstNumber && _isFirstNumber;
+            UpdateDisplay();
 
         }
 
         private void NumberButton9_Clicked(object sender, EventArgs e)
         {
-            _mainDisplayText += "9";
-            updateDisplay();
+            _mainDisplayText = _isFirstNumber ? "9": _mainDisplayText + "9";
+            _isFirstNumber = !_isFirstNumber && _isFirstNumber;
+            UpdateDisplay();
 
         }
 
         private void NumberButton0_Clicked(object sender, EventArgs e)
         {
-            _mainDisplayText += "0";
-            updateDisplay();
+            _mainDisplayText = _isFirstNumber ? "0": _mainDisplayText + "0";
+            _isFirstNumber = !_isFirstNumber && _isFirstNumber;
+            UpdateDisplay();
 
         }
 
         // This method is used to handle the event when the function buttons are clicked.
         private void ResetButton_Clicked(object sender, EventArgs e)
         {
-            _mainDisplayText = "";
-            _firstNumber = 0;
-            _secondNumber = 0;
-            _calculatingMethod = "";
-            updateDisplay();
+            this.Reset();
         }
 
         // This method is used to handle the event when the function buttons are clicked.
         private void FuntionButtonPlus_Clicked(object sender, EventArgs e)
         {
-            if (_firstNumber == 0)
+            
+            // check if there is a number to add
+            if(String.IsNullOrEmpty(_mainDisplayText))
             {
-                _firstNumber = int.Parse(_mainDisplayText);
+                System.Diagnostics.Debug.WriteLine("No number to add.");
+                return;
             }
 
+            // get number to add
+            int numberToAdd = int.Parse(_mainDisplayText);
+
+            // save the method and the number
             _calculatingMethod = "plus";
+            _totalNumber += numberToAdd;
 
-            _mainDisplayText = "";
-
-            //System.Diagnostics.Debug.WriteLine(firstNumber);
+            // reset the main display text
+            _mainDisplayText = $"{_totalNumber}";
+            _isFirstNumber = true;
+            UpdateDisplay();
         }
 
         // This method is used to handle the event when the - button is clicked.
         private void FunctionButtonMinus_Clicked(object sender, EventArgs e)
         {
-            if (_firstNumber == 0)
+            // check if there is a number to subtract
+            if (String.IsNullOrEmpty(_mainDisplayText))
             {
-                _firstNumber = int.Parse(_mainDisplayText);
+                System.Diagnostics.Debug.WriteLine("No number to subtract.");
+                return;
             }
 
-            _calculatingMethod = "plus";
+            // get number to add
+            int numberToSub = int.Parse(_mainDisplayText);
 
-            _mainDisplayText = "";
+            // save the method and the number
+            _calculatingMethod = "plus";
+            _totalNumber -= numberToSub;
+
+            // update the main display text
+            _mainDisplayText = $"{_totalNumber}";
+            _isFirstNumber = true;
+            UpdateDisplay();
         }
 
         // This method is used to handle the event when the = button is clicked.
         private void FunctionButtonEqual_Clicked(object sender, EventArgs e)
         {
-            if (_firstNumber == 0)
+            if (_totalNumber == 0)
             {
                 System.Diagnostics.Debug.WriteLine("There was no calculation function selected.");
                 return;
@@ -142,20 +178,35 @@
 
             if (_mainDisplayText == "")
             {
-                System.Diagnostics.Debug.WriteLine("Second number needed.");
+                System.Diagnostics.Debug.WriteLine("Input any numbers for calculation.");
                 return;
             }
 
-            _secondNumber = int.Parse(_mainDisplayText);
+            if (_calculatingMethod == "")
+            {
+                System.Diagnostics.Debug.WriteLine("No calculation method selected.");
+                return;
+            }
+
+            int numberToCalculate = int.Parse(_mainDisplayText);
 
             switch (_calculatingMethod)
             {
                 case "plus":
-                    int resultNumber = _firstNumber + _secondNumber;
-                    _mainDisplayText = $"{resultNumber}";
-                    updateDisplay();
+                    _totalNumber += numberToCalculate;
                     break;
+                case "minus":
+                    _totalNumber -= numberToCalculate;
+                    break;
+                default:
+                    return;
             }
+
+            // update the main display text
+            _mainDisplayText = $"{_totalNumber}";
+            _isFirstNumber = true;
+            _totalNumber = 0;
+            UpdateDisplay();
         }
     }
 
